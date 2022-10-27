76bdaeed0031738ad53d5ea1bfd7d253f8144563
Snehal jadhav <jsnehal217@gmail.com>
2022-10-27 16:10:03 +0530

    portfolio

---
+++ snehal.js
@@ -0,0 +1,17 @@
+var days= 9;
+var exp=5;
+var arr= [2,3,4,2,3,6,8,4,5];
+var notify=0;
+var median=0;
+var count=0;
+for(var i=0; i<arr.length; i++)
+  {    var bag=0;
+    for(var j=i; j<arr.length+1-exp; j++)
+      {
+        bag=bag+arr[j]
+        console.log(bag)
+      }
+    //console.log(bag)
+    
+      
+  }
